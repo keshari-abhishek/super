bfd43b0da7239d4c5bf3a4c317ff4beb78f34bd7
Abhishek <abhishekbca2k13@gmail.com>
2025-08-17 17:51:20 +0530

    Super App
    Super Project for DevOps and Full Stack Concept

---
+++ src/test/java/com/abhi/superapp/SuperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.superapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
